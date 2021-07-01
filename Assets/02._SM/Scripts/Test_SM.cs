fb3a1a719f0c86574eaa9908500207c4587a5595
ysm7280 <ysm7280@naver.com>
2021-07-01 17:30:42 +0900

    .

--- Assets/02._SM/Scripts/Test_SM.cs
+++ Assets/02._SM/Scripts/Test_SM.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test_SM : MonoBehaviour
 {
+    private void Awake()
+    {
+        //gameObject.SetActive(false);
+    }
+
     void Start()
     {
-        WebCamDevice[] devices = WebCamTexture.devices;
-        for (int i = 0; i < devices.Length; i++)
-        {
-            Debug.Log(devices[i].name);
-        }
-        print(devices[0]);
+        //PC버전
+#if UNITY_STANDALONE_WIN
+        print("standalone");
+        gameObject.SetActive(false);
+#endif
+
     }
 
     void Update()
