c9db07240dd44b5d88c3d22196faa04eb043448a
jinhyeok6941 <66781994+jinhyeok6941@users.noreply.github.com>
2021-07-05 15:32:49 +0900

    d

--- Assets/03_JH/Scripts/LineController.cs
+++ Assets/03_JH/Scripts/LineController.cs
@@ -14,28 +14,24 @@ public class LineController : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
-        //if (!photonView.IsMine) return;
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward);
-            if ((int)(pos.x * 10) > 2 && (int)(pos.y * 10) < 0)
+            if ((int)(pos.x * 10) > 1)
             {
                 use = false;
                 return;
             }
-            photonView.RPC("ClickDown", RpcTarget.All , pos , co.r , co.g , co.b , co.a  , width);
             ClickDown(pos, co.r, co.g, co.b, co.a, width);
         }
         else if (Input.GetMouseButton(0))
         {
             Vector3 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward);
-            photonView.RPC("ClickStay", RpcTarget.All , mpos);
             ClickStay(mpos);
         }
         else if (Input.GetMouseButtonUp(0))
         {
             use = true;
-            //photonView.RPC("ClickUp", RpcTarget.All);
         }
 
         if (Input.GetKey(KeyCode.Z))
@@ -46,6 +42,7 @@ public class LineController : MonoBehaviourPun
             {
                 Destroy(lrList[lrList.Count - 1]);
                 lrList.RemoveAt(lrList.Count - 1);
+                lrColor.RemoveAt(lrColor.Count - 1);
             }
         }
 
@@ -54,12 +51,7 @@ public class LineController : MonoBehaviourPun
             OnClickSend();
         }
     }
-    [PunRPC]
-    void SetUse()
-    {
-        use = false;
-    }
-    [PunRPC]
+    
     void ClickDown(Vector3 pos, float r, float g, float b, float a, float width)
     {
         if (!use) return;
@@ -69,51 +61,35 @@ public class LineController : MonoBehaviourPun
         LineSetting(new Color(r, g, b, a), width);
         lrColor.Add(new Color(r, g, b, a));
         lrList.Add(go);
-        lr.SetPosition(0, pos /*+ Camera.main.transform.position + Camera.main.transform.forward * 0.1f*/);
+        lr.SetPosition(0, pos);
     }
-    [PunRPC]
     void ClickStay(Vector3 mpos)
     {
         if (!use) return;
         Vector3 lrpos = lrList[lrList.Count - 1].GetComponent<LineRenderer>().GetPosition(lr.positionCount - 1);
         if (Vector3.Distance(lrpos, mpos) < 0.005f) return;
         lr.positionCount++;
-        lr.SetPosition(lr.positionCount - 1, mpos /*+ Camera.main.transform.position + Camera.main.transform.forward * 0.1f*/);
-    }
-    void ClickUp()
-    {
-        use = true;
-    }
-
-    public void SendLineTest()
-    {
-        if (lrList.Count == 0) return;
-        //CreateLine();
-        LineRenderer line = lrList[lrList.Count - 1].GetComponent<LineRenderer>();
-        for (int i = 0; i < line.positionCount; i++)
-        {
-            lr.positionCount++;
-            lr.SetPosition(i, line.GetPosition(i) + Camera.main.transform.position);
-        }
+        lr.SetPosition(lr.positionCount - 1, mpos);
     }
 
     public void OnClickSend()
     {
-        //if (!photonView.IsMine) return;
         if (lrList.Count == 0) return;
         for (int i = 0; i < lrList.Count; i++)
         {
-            photonView.RPC("CreateLine", RpcTarget.Others , lrColor[i].r , lrColor[i].g , lrColor[i].b , lrColor[i].a);
+            photonView.RPC("CreateLine", RpcTarget.All , lrColor[i].r , lrColor[i].g , lrColor[i].b , lrColor[i].a);
             LineRenderer line = lrList[i].GetComponent<LineRenderer>();
             for (int j = 0; j < line.positionCount; j++)
-                photonView.RPC("SetLinePos", RpcTarget.Others, line.GetPosition(j), j);
+                photonView.RPC("SetLinePos", RpcTarget.All, line.GetPosition(j), j);
         }
     }
     [PunRPC]
     void SetLinePos(Vector3 pos, int i)
     {
         lr.positionCount++;
-        lr.SetPosition(i, pos + Camera.main.transform.position + Camera.main.transform.forward * 0.1f);
+        lr.SetPosition(i, pos);
+        lr.transform.position = Camera.main.transform.position;
+        lr.transform.forward = Camera.main.transform.forward;
     }
     [PunRPC]
     //라인 생성
