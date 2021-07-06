8e6e5227a941afc04b5f9369f782b347147d2456
diceyu <diceyu@gmail.com>
2021-07-06 16:49:26 +0900

    1

---
+++ Assets/04._KR/01_Scripts/SpeakerManager_KR.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpeakerManager_KR : MonoBehaviourPun
+{
+
+    void Start()
+    {
+        if(photonView.IsMine)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            GetComponent<AudioListener>().enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
