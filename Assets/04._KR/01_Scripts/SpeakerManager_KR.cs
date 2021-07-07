c79d387c4da1558e5846484fe441051982e9b365
diceyu <diceyu@gmail.com>
2021-07-07 14:18:12 +0900

    1

--- Assets/04._KR/01_Scripts/SpeakerManager_KR.cs
+++ Assets/04._KR/01_Scripts/SpeakerManager_KR.cs
@@ -2,18 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using Photon.Voice.Unity;
+using Photon.Voice.PUN;
 
 public class SpeakerManager_KR : MonoBehaviourPun
 {
+    public KeyCode pushBtn = KeyCode.P;
+    public Recorder voiceRecorder;
+    PhotonView view;
 
     void Start()
     {
-        AudioListener.volume = 1;
-        
+        view = photonView;
+        voiceRecorder.TransmitEnabled = false;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(pushBtn))
+        {
+            if(view.IsMine)
+            {
+                voiceRecorder.TransmitEnabled = true;
+            }
+        }
+        else if(Input.GetKeyUp(pushBtn))
+        {
+            if (view.IsMine)
+            {
+                voiceRecorder.TransmitEnabled = false;
+            }
+        }
     }
 }
