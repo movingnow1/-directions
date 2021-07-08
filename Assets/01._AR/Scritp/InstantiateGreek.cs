3bbe14792a5f1f69979941a5466fbf029114cbec
movingnow1 <choice3798@naver.com>
2021-07-08 10:57:16 +0900

    .

---
+++ Assets/01._AR/Scritp/InstantiateGreek.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateGreek : MonoBehaviour
+{
+    public GameObject instantiateHouse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void seeHouse()
+    {
+        GameObject house=Instantiate(instantiateHouse);
+        house.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+    }
+}
